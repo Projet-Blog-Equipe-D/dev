ba4ec170fe352449a21b61510f6698641ac6dc05
Pierrot <pierrot63200@mail.com>
2017-11-20 10:08:01 +0100

    Division de index
    - Division de index.php grâce à des 'include'

---
+++ header.php
@@ -0,0 +1,19 @@
+<?php 
+	session_start();
+	include_once "inc/pdo.php";
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<meta charset="utf-8">
+		
+		<!-- Importation BootStrap 3 -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+	
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		
+		<!-- Importation de jQuery -->
+		<script src="js/jquery-3.2.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</head>
