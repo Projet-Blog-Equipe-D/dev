f4dd0bba56b1f7ce5509075bbd6fdcc80ac5818f
Pierrot <pierrot63200@mail.com>
2017-11-20 10:08:31 +0100

    Division de la connexion à la base de données
    - Division de la connexion à la base de données grâce à un include (header.php)

---
+++ inc/pdo.php
@@ -0,0 +1,23 @@
+<?php
+	try{
+		$db_config = array();
+		$db_config['SGBD']	= 'mysql';
+		$db_config['HOST']	= 'localhost';
+		$db_config['DB_NAME']	= 'contact';
+		$db_config['USER']	= 'root';
+		$db_config['PASSWORD']	= '';
+		$db_config['OPTIONS']	= array(
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+		);
+		$pdo = new PDO(
+			$db_config['SGBD'] .':host='. $db_config['HOST'] .';dbname='. $db_config['DB_NAME'],
+			$db_config['USER'],
+			$db_config['PASSWORD'],
+			$db_config['OPTIONS']);
+		unset($db_config);
+	}
+	catch(Exception $e){
+		trigger_error($e->getMessage(), E_USER_ERROR);
+	}
+?>
