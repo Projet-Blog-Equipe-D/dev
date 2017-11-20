696ac7bd5851142884f717470748e77d568ab661
Pierrot <pierrot63200@mail.com>
2017-11-20 16:56:47 +0100

    Mise à jour CSS + Images bannière
    Mise à jour CSS + Images bannière

--- header.php
+++ header.php
@@ -12,8 +12,9 @@
 		
 		<!-- Importation BootStrap 3 -->
 		<link href="css/bootstrap.min.css" rel="stylesheet">
-	
+		
 		<link rel="stylesheet" type="text/css" href="css/style.css">
+		
 		
 		<!-- Importation de jQuery -->
 		<script src="js/jquery-3.2.1.min.js"></script>
