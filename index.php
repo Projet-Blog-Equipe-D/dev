6515f7991193f64a868356d0ddc57a10b3079f6f
Pierrot <pierrot63200@mail.com>
2017-11-20 11:19:57 +0100

    Navbar de base
    Navbar de base

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
 <?php include_once "header.php"; ?>
 
 	<body>
+		<header>
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="collapsed navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
+					</button>
+				</div> 
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse"> 
+					<form class="navbar-form navbar-left">
+						<div class="form-group">
+							<!-- Menu selection dynamique (catégories) en php -->
+						</div> 
+						<button type="submit" class="btn btn-default">Filtrer</button>
+					</form>
+					<ul class="navbar-right">
+						<li><a href="#">CONNEXION</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		</header>
 
 
 		<script type="text/javascript" src="js/main.js"></script>
