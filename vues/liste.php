22873261f354dfd0a76aff6acd3b20b19f0c18ad
Valentin <valentinlaidet1@gmail.com>
2017-11-21 09:18:18 +0100

    Ajout du css article

--- vues/liste.php
+++ vues/liste.php
@@ -3,11 +3,16 @@
 		<h1><?php echo $titre; ?></h1>
 		<?php
 			foreach ($liste as $art) {
-				echo '<article id="article-'.$art->getIdArticle().'">';
-				// echo $art->getIdArticle();
-				echo $art->getTitre();
-				echo $art->getContenu();
-				echo "</article>";
+				echo "<section class='container'>";
+					echo "<div class='row'>";
+					echo "<div class='col-md-10 col-xs-10 col-lg-10 col-sm-10'>";
+						echo '<article class="article" id="article-'.$art->getIdArticle().'">';
+							// echo $art->getIdArticle();
+							echo $art->getTitre();
+							echo $art->getContenu();
+						echo "</article>";
+					echo "</div>";
+				echo "</section>";
 			}
 
 		?>
